97d7a121d7b73c2ab83b204492c2b165db3b3af5
joao.vsantos92 <joao.vsantos92@CAR0717579W11-1>
2025-06-24 14:04:22 -0300

    fiz a estrutura da aula passada que eu tinha faltado

---
+++ JogoConsole/JogoConsole/Guerra Espacial.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoConsole
+{
+    internal class Guerra_Espacial
+    {
+        class GuerraEspacial
+        {
+            static char[,] mapa;
+            static int altura = 10;
+            static int largura = 20;
+            static int playerx = 1;
+            static int playery = 1;
+            static bool jogando = true;
+
+            static void Main()
+
+            {
+
+                Console.Clear();
+
+                Jogar();
+
+
+
+            }
+            static void Jogar()
+            {
+                iniciarmapa();
+
+
+                while (jogando)
+                {
+                    Console.Clear();
+                    DesenhandoMapa();
+                    var tecla = Console.ReadKey(true).Key;
+                    AtualizarPosicao(tecla);
+
+
+
+                }
+
+            }
+
+            static void iniciarmapa()
+            {
+                mapa = new char[largura, altura];
+                for (int x = 0; x < largura; x++)
+                {
+                    for (int y = 0; y < altura; y++)
+                    {
+                        if (x == 0 || x == largura - 1 || y == 0 || y == altura - 1)
+                        {
+                            mapa[x, y] = '#'; // paredes
+                        }
+                        else
+                        {
+                            mapa[x, y] = ' '; // espaço vazio
+                        }
+                    }
+                }
+
+                mapa[playerx, playery] = '@'; // posição do jogador
+            }
+
+            static void DesenhandoMapa()
+            {
+
+                for (int y = 0; y < altura; y++)
+                {
+                    for (int x = 0; x < largura; x++)
+                    {
+
+                        Console.Write(mapa[x, y]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            static void AtualizarPosicao(ConsoleKey tecla)
+            {
+                int x = playerx;
+                int y = playery;
+
+                switch (tecla)
+                {
+                    case ConsoleKey.W: // cima
+                        y--;
+                        break;
+                    case ConsoleKey.S: // baixo
+                        y++;
+                        break;
+                    case ConsoleKey.A: // esquerda
+                        x--;
+                        break;
+                    case ConsoleKey.D: // direita
+                        x++;
+                        break;
+
+                }
+
+                if (mapa[x, y] != '#')
+                {
+                    mapa[playerx, playery] = ' ';
+                    mapa[x, y] = '@';
+                    playerx = x;
+                    playery = y;
+
+                }
+            }
+
+        }
+    
---
+++ JogoConsole/JogoConsole/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Objetivo: O jogador controla uma nave (^) que pode se mover na horizontal e atirar em inimigos (X).
+Controles: A / D para mover, Espaço para atirar.
+Inimigos caem de cima para baixo.
+Tiros eliminam inimigos.
+Se nao conseguir eliminar um inimigo e ele tocar na borda, O jogador perde 1 vida.
+*/
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+//char inimigo;
+//char nave;
+//int vida = 3;
+//double dano = 1;
+
+
+/*
+/ / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / 
+/                                                                                                   /
+/                                                                                                   /
+/                                                                                                   /
+/                                                                                                   /
+/                                                                                                   /
+/                                                                                                   /
+/                                                                                                   /
+/                                                                                                   /
+/                                                                                                   /
+/                               X             X           X            X                            /
+/                                                                                                   /
+/                                                                                                   /
+/                                                                                                   /
+/                                                                                                   /
+/                                                                                                   /
+/                                                                                                   /
+/                                                                                                   /
+/                                                                                                   /
+/                                                                                                   /
+/                                                 ^                                                 /
+/                                                                                                   /
+/                                                                                                   /
+/                                                                                                   /
+/ / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / 
+*/
+
+
+Console.WriteLine("Guerra Espacial");
+
+
+
+Console.WriteLine("/ / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / \r\n/                                                                                                   /\r\n/                                                                                                   /\r\n/                                                                                                   /\r\n/                                                                                                   /\r\n/                                                                                                   /\r\n/                                                                                                   /\r\n/                                                                                                   /\r\n/                                                                                                   /\r\n/                                                                                                   /\r\n/                               X             X           X            X                            /\r\n/                                                                                                   /\r\n/                                                                                                   /\r\n/                                                                                                   /\r\n/                                                                                                   /\r\n/                                                                                                   /\r\n/                                                                                                   /\r\n/                                                                                                   /\r\n/                                                                                                   /\r\n/                                                                                                   /\r\n/                                                 ^                                                 /\r\n/                                                                                                   /\r\n/                                                                                                   /\r\n/                                                                                                   /\r\n/ / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / \r\n");
+
+
