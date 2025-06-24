b051867c6e36372feae7e002d59ce2878af395f6
joao.vsantos92 <joao.vsantos92@CAR0717579W11-1>
2025-06-24 16:27:39 -0300

    inimigo se mexendo para baixo quando o player se move e tirando 1 vida quando bate na borda do mapa

--- JogoConsole/JogoConsole/Guerra Espacial.cs
+++ JogoConsole/JogoConsole/Guerra Espacial.cs
@@ -1,106 +1,145 @@
 ﻿using System;
+using System.Reflection.PortableExecutable;
 
 namespace JogoConsole
 {
-    
-    
-        class GuerraEspacial
+
+
+    class GuerraEspacial
+    {
+        static int vida = 3;
+        static char[,] mapa;
+        static int altura = 20;
+        static int largura = 50;
+        static int playerX = 25;
+        static int playerY = 15;
+        static int inimigoX = 10;
+        static int inimigoY = 5;
+        static int tiroY = 0;
+        static int tiroX = 0;
+        static bool jogando = true;
+
+        public static void Main()
+
         {
-            static char[,] mapa;
-            static int altura = 20;
-            static int largura = 50;
-            static int playerX = 25;
-            static int playerY = 15;
-            static bool jogando = true;
 
-           public static void Main()
+            Console.Clear();
 
+            Jogar();
+
+
+
+        }
+        public static void Jogar()
+        {
+            iniciarmapa();
+
+
+            while (jogando)
             {
-
                 Console.Clear();
+                DesenhandoMapa();
+                var tecla = Console.ReadKey(true).Key;
+                AtualizarPosicao(tecla);
 
-                Jogar();
-
-
-
-            }
-           public static void Jogar()
-            {
-                iniciarmapa();
-
-
-                while (jogando)
+                if (vida < 1)
                 {
-                    Console.Clear();
-                    DesenhandoMapa();
-                    var tecla = Console.ReadKey(true).Key;
-                    AtualizarPosicao(tecla);
-
-
-
+                    jogando = false;
                 }
 
+
             }
 
-            static void iniciarmapa()
+        }
+
+        static void iniciarmapa()
+        {
+            mapa = new char[largura, altura];
+            for (int x = 0; x < largura; x++)
             {
-                mapa = new char[largura, altura];
-                for (int x = 0; x < largura; x++)
-                {
-                    for (int y = 0; y < altura; y++)
-                    {
-                        if (x == 0 || x == largura - 1 || y == 0 || y == altura - 1)
-                        {
-                            mapa[x, y] = '/'; // paredes
-                        }
-                        else
-                        {
-                            mapa[x, y] = ' '; // espaço vazio
-                        }
-                    }
-                }
-
-         
-                mapa[playerX, playerY] = '^'; // posição do jogador
-            }
-
-            static void DesenhandoMapa()
-            {
-
                 for (int y = 0; y < altura; y++)
                 {
-                    for (int x = 0; x < largura; x++)
+                    if (x == 0 || x == largura - 1 || y == 0 || y == altura - 1)
                     {
-
-                        Console.Write(mapa[x, y]);
+                        mapa[x, y] = '/'; // paredes
                     }
-                    Console.WriteLine();
+                    else
+                    {
+                        mapa[x, y] = ' '; // espaço vazio
+                    }
                 }
             }
 
-            static void AtualizarPosicao(ConsoleKey tecla)
+            mapa[tiroX, tiroY] = '.';
+            mapa[inimigoX, inimigoY] = 'X';
+            mapa[playerX, playerY] = '^'; // posição do jogador
+        }
+
+        static void DesenhandoMapa()
+        {
+
+            for (int y = 0; y < altura; y++)
             {
-                int tempX = playerX;
-                int tempY = playerY;
-
-                switch (tecla)
+                for (int x = 0; x < largura; x++)
                 {
-                    case ConsoleKey.A: // esuqerda
-                        tempX--;
-                        break;
-                    case ConsoleKey.S: // baixo
-                        tempY++;
-                        break;
-                    case ConsoleKey.D: // direita
-                        tempX++;
-                        break;
-                    case ConsoleKey.W: // cima-
-                        tempY--;
-                        break;
 
+                    Console.Write(mapa[x, y]);
                 }
+                Console.WriteLine();
+            }
+        }
 
-                if (mapa[tempX, tempY] != '/')
+        static void AtualizarPosicao(ConsoleKey tecla)
+        {
+            int tempX = playerX;
+            int tempY = playerY;
+            int troY = tiroY;
+
+            switch (tecla)
+            {
+                case ConsoleKey.A: // esuqerda
+                    tempX--;
+                    break;
+                case ConsoleKey.S: // baixo
+                    tempY++;
+                    break;
+                case ConsoleKey.D: // direita
+                    tempX++;
+                    break;
+                case ConsoleKey.W: // cima-
+                    tempY--;
+                    break;
+                case ConsoleKey.E: // atirar
+                    troY++;
+                    break;
+
+            }
+
+            int eniX = inimigoX;
+            int eniY = inimigoY+1;
+
+            Random random = new Random();  
+
+            if (eniY < altura)
+            { 
+                inimigoY++;
+                mapa[inimigoX, inimigoY - 1] = ' ';
+                mapa[inimigoX, inimigoY] = 'X';
+            } else {
+                mapa[inimigoX, inimigoY] = '/';
+                vida--;
+                inimigoY = 5;
+                inimigoX = random.Next(2,largura);
+
+            }
+
+
+
+
+
+
+
+            if (mapa[tempX, tempY] != '/')
                 {
                     mapa[playerX, playerY] = ' ';
                     mapa[tempX, tempY] = '^';
@@ -108,10 +147,11 @@ namespace JogoConsole
                     playerY = tempY;
 
                 }
-            }
+      
 
         }
     }
+}
 
 
 
